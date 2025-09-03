d21d8a5551f4f67c5faccb05293335c5bc2a0a61
yusen-luan <luanyusen@gmail.com>
2025-09-03 11:29:04 +0800

    Add basic GUI with JavaFX

---
+++ src/main/java/pazuzu/Launcher.java
@@ -0,0 +1,13 @@
+package pazuzu;
+import javafx.application.Application;
+import pazuzu.ui.Main;
+
+/**
+ * A launcher class to workaround classpath issues.
+ */
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(Main.class, args);
+    }
+}
+
